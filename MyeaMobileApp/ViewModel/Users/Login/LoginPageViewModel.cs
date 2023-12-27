46487799d2d6b40fc73da28c1afe4377e125195e
Thomas Brockington <xtombrock1989@gmail.com>
2023-12-27 04:08:21 +0000

    login or reg

--- MyeaMobileApp/ViewModel/Users/Login/LoginPageViewModel.cs
+++ MyeaMobileApp/ViewModel/Users/Login/LoginPageViewModel.cs
@@ -1,9 +1,14 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace MyeaMobileApp.ViewModel.Users.Login
 {
     public partial class LoginPageViewModel : ObservableObject
     {
-
+        [RelayCommand]
+        public async Task NavigateToRegisterPage()
+        {
+            await Shell.Current.GoToAsync("///RegisterPage");
+        }
     }
 }
--- MyeaMobileApp/ViewModel/Users/LoginOrRegister/LoginOrRegisterPageViewModel.cs
+++ MyeaMobileApp/ViewModel/Users/LoginOrRegister/LoginOrRegisterPageViewModel.cs
@@ -1,8 +1,14 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace MyeaMobileApp.ViewModel.Users.LoginOrRegister
 {
     public partial class LoginOrRegisterPageViewModel : ObservableObject
     {
+        [RelayCommand]
+        public async Task NavigateToRegisterPage()
+        {
+            await Shell.Current.GoToAsync("///RegisterPage");
+        }
     }
 }
